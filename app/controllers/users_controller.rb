3f0fda3da962df378797938adcb689d041d9f61d
nagisa55 <kamomeeee00@yahoo.co.jp>
2021-02-17 23:20:35 +0000

    3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       redirect_to :new
+    end
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
-  resources :users
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create]
+
+
 end
