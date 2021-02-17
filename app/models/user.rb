5db0449f36ed01e2d613ff51320867b681b73521
nagisa55 <kamomeeee00@yahoo.co.jp>
2021-02-17 12:30:00 +0000

    2

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 10 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  validates :profile, length: { maximum: 250 }
+  validates :good_point, length: { maximum: 100 }
+  has_secure_password
+end
