7247a562019e63b931e91fc45e9ba992ecadf4ee
github nagisa55 <kamomeeee00@yahoo.co.jp>
2021-02-21 21:02:58 +0900

    9

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,19 @@ class Post < ApplicationRecord
   validates :category, presence: true
   validates :title, presence: true, length: { maximum:50 }
   validates :content, presence: true, length: { maximum:500 }
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
+
+  def like(user)
+    favorites.create(user_id: user.id)
+  end
+
+  def unlike(user)
+    favorites.find_by(user_id: user.id).destroy
+  end
+
+  def like?(user)
+    favorite_users.include?(user)
+  end
 end
