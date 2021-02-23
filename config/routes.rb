7a0624ab1b7a834afa046b0816fce633ea0645c4
github nagisa55 <kamomeeee00@yahoo.co.jp>
2021-02-23 17:16:25 +0900

    11

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
 
   get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :create, :edit]
   resources :posts
 
 end
